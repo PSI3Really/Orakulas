ca6df13efbcd7568d253bc5b28f153c15131a7de
Kestutis Baltronas <fidzinius@gmail.com>
2011-11-20 15:27:56 +0200

    admin

---
+++ web/js/app/view/Admin/Users/UsersGrid.js
@@ -0,0 +1,19 @@
+Ext.define(CONFIG.APP_NS+'.view.Admin.Users.UsersGrid', {
+    extend: 'Ext.grid.Panel',
+    alias: 'widget.adminusersgrid',
+
+    initComponent: function() {
+        this.store = {
+            fields: ['name', 'email', 'veiksmai'],
+            data  : []
+        };
+        
+        this.columns = [
+            {header: 'Vartotojo vardas',  dataIndex: 'name',  flex: 1},
+            {header: 'Vartotojo lenteles', dataIndex: 'email', flex: 1},
+            {header: 'Veiksmai', dataIndex: 'veiksmai', flex: 1}
+        ];
+
+        this.callParent(arguments);
+    }
+});
---
+++ web/js/app/view/Admin/Window.js
@@ -0,0 +1,26 @@
+Ext.define(CONFIG.APP_NS+'.view.Admin.Window', {
+    extend: 'Ext.window.Window',
+    alias:  'widget.adminWindow',
+
+    layout: 'border',
+    floatable: true,
+    modal: true,
+
+    height: 500,
+    width: 600,
+    title: 'Administravimas / Nustatymai',
+
+    requires: [
+        CONFIG.APP_NS+'.view.Admin.TabPanel'
+    ],
+
+    initComponent: function() {
+        this.items = [
+            {
+                region: 'center',
+                xtype: 'admintabpanel'
+            }
+        ];
+        this.callParent(arguments);
+    }
+});
