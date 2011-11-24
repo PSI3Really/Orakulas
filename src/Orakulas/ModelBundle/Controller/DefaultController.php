d747e0b7db42c93d15c0506fd12737751c7e79c0
Adomas Zitkevicius <adozit@gmail.com>
2011-11-24 14:06:15 +0200

    Pridetas deleteAll()

--- src/Orakulas/ModelBundle/Controller/DefaultController.php
+++ src/Orakulas/ModelBundle/Controller/DefaultController.php
@@ -19,15 +19,14 @@ class DefaultController extends Controller
 
     private function getJsonResponse($class, $id = -1)
     {
-        $this->initEntityFacade();
         $obj = 0;
         if ($id < 0)
         {
-            $obj = $this->entityFacade->loadAll($class);
+            $obj = $this->getEntityFacade()->loadAll($class);
         }
         else
         {
-            $obj = $this->entityFacade->load($class, $id);
+            $obj = $this->getEntityFacade()->load($class, $id);
         }
 
         $response = new Response(ModelUtils::jsonEncodeEx($obj));
@@ -38,18 +37,17 @@ class DefaultController extends Controller
 
     public function departmentAction($id = -1)
     {
-            return $this->getJsonResponse(EntityFacade::DEPARTMENT, $id);
+        return $this->getJsonResponse(EntityFacade::DEPARTMENT, $id);
     }
 
     public function departmentPostAction() {
-        $this->initEntityFacade();
         $this->postValue(EntityFacade::DEPARTMENT);
         exit;
     }
 
-    public function departmentPutAction($id = -1) {
-        { echo '<pre>';print_r($_POST);exit; }
-        echo $id;
+    public function departmentDeleteAction() {
+        $this->deleteAll(EntityFacade::DEPARTMENT);
+        exit;
     }
 
     public function departmentInfoSysUsageAction($id = -1)
@@ -58,8 +56,12 @@ class DefaultController extends Controller
     }
 
     public function departmentInfoSysUsagePostAction() {
-        $this->initEntityFacade();
-        $this->postValue(EntityFacade::DEPARTMENT_INFO_SYS_USAGEs);
+        $this->postValue(EntityFacade::DEPARTMENT_INFO_SYS_USAGE);
+        exit;
+    }
+
+    public function departmentInfoSysUsageDeleteAction() {
+        $this->deleteAll(EntityFacade::DEPARTMENT_INFO_SYS_USAGE);
         exit;
     }
 
@@ -69,8 +71,12 @@ class DefaultController extends Controller
     }
 
     public function informationalSystemPostAction() {
-        $this->initEntityFacade();
         $this->postValue(EntityFacade::INFORMATIONAL_SYSTEM);
+        exit;
+    }
+
+    public function informationalSystemDeleteAction() {
+        $this->deleteAll(EntityFacade::INFORMATIONAL_SYSTEM);
         exit;
     }
 
@@ -80,8 +86,12 @@ class DefaultController extends Controller
     }
 
     public function supportAdministrationTimePostAction() {
-        $this->initEntityFacade();
         $this->postValue(EntityFacade::SUPPORT_ADMINISTRATION_TIME);
+        exit;
+    }
+
+    public function supportAdministrationTimeDeleteAction() {
+        $this->deleteAll(EntityFacade::SUPPORT_ADMINISTRATION_TIME);
         exit;
     }
 
@@ -91,8 +101,12 @@ class DefaultController extends Controller
     }
 
     public function supportCategoryPostAction() {
-        $this->initEntityFacade();
         $this->postValue(EntityFacade::SUPPORT_CATEGORY);
+        exit;
+    }
+
+    public function supportCategoryDeleteAction() {
+        $this->deleteAll(EntityFacade::SUPPORT_CATEGORY);
         exit;
     }
 
@@ -102,9 +116,13 @@ class DefaultController extends Controller
         return $this->getJsonResponse(EntityFacade::SUPPORT_HISTORY, $id);
     }
 
-    public function supportHistoryPost() {
-        $this->initEntityFacade();
+    public function supportHistoryPostAction() {
         $this->postValue(EntityFacade::SUPPORT_HISTORY);
+        exit;
+    }
+
+    public function supportHistoryDeleteAction() {
+        $this->deleteAll(EntityFacade::SUPPORT_HISTORY);
         exit;
     }
 
@@ -114,8 +132,12 @@ class DefaultController extends Controller
     }
 
     public function supportTypePostAction() {
-        $this->initEntityFacade();
         $this->postValue(EntityFacade::SUPPORT_TYPE);
+        exit;
+    }
+
+    public function supportTypeDeleteAction() {
+        $this->deleteAll(EntityFacade::SUPPORT_TYPE);
         exit;
     }
 
@@ -125,22 +147,31 @@ class DefaultController extends Controller
     }
 
     public function userPostAction() {
-        $this->initEntityFacade();
         $this->postValue(EntityFacade::USER);
         exit;
     }
 
-    private function initEntityFacade() {
-        if ($this->entityFacade == null) {
+    public function userDeleteAction() {
+        $this->deleteAll(EntityFacade::USER);
+        exit;
+    }
+
+    private function getEntityFacade() {
+        if (!isset($this->entityFacade)) {
             $this->entityFacade = new EntityFacade();
             $this->entityFacade->setDoctrine($this->getDoctrine());
         }
+        return $this->entityFacade;
     }
 
     private function postValue($className) {
         $jsonValue = $_POST["jsonValue"];
         $jsonValueDecoded = json_decode($jsonValue, true);
         $jsonValueObject = ModelUtils::arrayToObject($jsonValueDecoded, $className);
-        $this->entityFacade->save($jsonValueObject);
+        $this->getEntityFacade()->save($jsonValueObject);
+    }
+
+    private function deleteAll($className) {
+        $this->getEntityFacade()->deleteAll($className);
     }
 }
