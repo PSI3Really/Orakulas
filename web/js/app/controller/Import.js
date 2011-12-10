e5c952a0d6d2d30cb7a7a001b0adf1dfcbabbd5e
Aleksandras Nelkinas <a.nelkinas@gmail.com>
2011-12-10 16:50:40 +0200

    File upload in data import

--- web/js/app/controller/Import.js
+++ web/js/app/controller/Import.js
@@ -27,6 +27,41 @@ Ext.define(CONFIG.APP_NS+'.controller.Import', {
     },
 
     openFile: function(field, value){
-        alert('Open file: ' + value);
+        var msg = function(title, msg, icon) {
+            Ext.Msg.show({
+                title: title,
+                msg: msg,
+                minWidth: 200,
+                modal: true,
+                icon: icon,
+                buttons: Ext.Msg.OK
+            });
+        };
+
+        var form = field.up('form').getForm();
+        if (form.isValid()) {
+            form.submit({
+                url: 'excel/import/supportHistories',
+                waitMsg: '~~Duomenų failas saugomas ir skaitomas&hellip;',
+                success: function(form, action) {
+                    msg('Data Received', 'Populate the grid with action.result.data array', Ext.Msg.INFO);
+                },
+                failure: function(form, action) {
+                    var message = '~~Nepavyko perskaityti duomenų failo.'; // unknown error
+                    switch (action.result.errors) {
+                        case 'INVALID_FILE_TYPE':
+                            message = '~~Klaidingas duomenų failo formatas. Turėtų būti XLS arba XLSX.';
+                            break;
+                        case 'NO_SUCH_SHEET':
+                            message = '~~Tokio puslapio duomenų faile nėra.';
+                            break;
+                        case 'INVALID_DATA':
+                            message = '~~Klaidingai suformuotas duomenų failas.';
+                            break;
+                    }
+                    msg('~~Įvyko klaida', message, Ext.Msg.ERROR);
+                }
+            });
+        }
     }
 });
--- web/js/app/view/Import/Toolbar.js
+++ web/js/app/view/Import/Toolbar.js
@@ -4,25 +4,35 @@ Ext.define(CONFIG.APP_NS+'.view.Import.Toolbar', {
 
     border: false,
 
-    initComponent: function(){
-        this.items = [{
-            xtype: 'filefield', //TODO: make every button look the same
-            action: 'openFile',
-            buttonOnly: true,
-            hideLabel: true,
-            buttonText: LANG.BUTTON.OPEN_FILE,
-            buttonConfig:{
-                iconCls: 'icon-folder-open'
+    initComponent: function() {
+        this.items = [
+            {
+                cls: "dummy-form",
+                xtype: 'form',
+                items: [{
+                    xtype: 'filefield', //TODO: make every button look the same
+                    name: 'dataFile',
+                    action: 'openFile',
+                    buttonOnly: true,
+                    hideLabel: true,
+                    buttonText: LANG.BUTTON.OPEN_FILE,
+                    buttonConfig: {
+                        iconCls: 'icon-folder-open'
+                    }
+                }]
+            },
+            '->',
+            {
+                xtype: 'button',
+                action: 'accept',
+                text: LANG.BUTTON.OK
+            },
+            {
+                xtype: 'button',
+                action: 'cancel',
+                text: LANG.BUTTON.CANCEL
             }
-        },'->',{
-            xtype: 'button',
-            action: 'accept',
-            text: LANG.BUTTON.OK
-        },{
-            xtype: 'button',
-            action: 'cancel',
-            text: LANG.BUTTON.CANCEL
-        }];
+        ];
 
         this.callParent(arguments);
     }
